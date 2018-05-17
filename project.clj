c120455415429312c6b0ee428551748f6ac62ba7
Johannes Tax <johannes@johannes.tax>
2018-05-17 19:08:49 +0200

    Test and coverage reports in Circle CI.
    Relates to #14.

--- project.clj
+++ project.clj
@@ -6,4 +6,6 @@
   :main ^:skip-aot vocabula.core
   :target-path "target/%s"
   :test-paths ["src" "test"]
+  :plugins [[lein-cloverage "1.0.10"]
+            [lein-test-report-junit-xml "0.2.0"]]
   :profiles {:uberjar {:aot :all}})
